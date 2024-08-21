82e19504754047492ea1927bdbdb688e7b617490
Juan-Alfredo <79122698+Juan-Alfredo@users.noreply.github.com>
2024-08-21 13:24:22 -0500

    Cambio-de-conexion-api
    Se cambio la pagina de conexion api

--- Reporte_Fallas.js
+++ Reporte_Fallas.js
@@ -104,137 +104,131 @@ function ValidarForm() {
     if (inputs[i].value.trim() === "") {
       alert("Por favor, completa todos los campos.");
       return false; // Detiene el envío del formulario
-    } else {
-      Registrar();
     }
   }
 
-  async function Registrar() {
-    var codigo_unidad = document.getElementById("codigo_unidad").value;
-    var placa_remolque = document.getElementById("placa_remolque").value;
-    var codigo_acople = document.getElementById("codigo_acople").value;
-    var placa_semirremolque = document.getElementById(
-      "placa_semirremolque"
-    ).value;
-    var nombre_operador = document.getElementById("nombre_operador").value;
-    var celular_operador = document.getElementById("celular_operador").value;
-    var tipo_unidad = document.getElementById("tipo_unidad").value;
-    var tipo_unidadparticular = document.getElementById(
-      "tipo_unidadparticular"
-    ).value;
-    var estado_carga = document.getElementById("estado_carga").value;
-    var tipo_carga = document.getElementById("tipo_carga").value;
-    var origen = document.getElementById("origen").value;
-    var destino = document.getElementById("destino").value;
-    var fecha_ocurrencia = document.getElementById("fecha_ocurrencia").value;
-    var hora_ocurrencia = document.getElementById("hora_ocurrencia").value;
-    var fecha_reinicio = document.getElementById("fecha_reinicio").value;
-    var hora_reinicio = document.getElementById("hora_reinicio").value;
-    var tiempo_detenido = document.getElementById("tiempo_detenido").value;
-    var tipo_falla = document.getElementById("tipo_falla").value;
-    var lugar_ocurrencia = document.getElementById("lugar_ocurrencia").value;
-    var zona = document.getElementById("zona").value;
-    var sede = document.getElementById("sede").value;
-    var region = document.getElementById("region").value;
-    var provincia = document.getElementById("provincia").value;
-    var distrito = document.getElementById("distrito").value;
-    var latitud = document.getElementById("latitud").value;
-    var longitud = document.getElementById("longitud").value;
-    var inicio_tramo = document.getElementById("inicio_tramo").value;
-    var fin_tramo = document.getElementById("fin_tramo").value;
-    var personal_informante = document.getElementById(
-      "personal_informante"
-    ).value;
-    var gestor = document.getElementById("gestor").value;
-    var fecha_recepcion_informacion = document.getElementById(
-      "fecha_recepcion_informacion"
-    ).value;
-    var hora_recepcion_informacion = document.getElementById(
-      "hora_recepcion_informacion"
-    ).value;
-    var ubicacion_actual = document.getElementById("ubicacion_actual").value;
-    var detalle_clima = document.getElementById("detalle_clima").value;
+  Registrar();
+  return true;
+}
 
-    var zona_peligrosa = document.getElementById("Zona_Peligrosa_Si").checked
-      ? "SI"
-      : document.getElementById("Zona_Peligrosa_No").checked
+async function Registrar() {
+  var codigo_unidad = document.getElementById("codigo_unidad").value;
+  var placa_remolque = document.getElementById("placa_remolque").value;
+  var codigo_acople = document.getElementById("codigo_acople").value;
+  var placa_semirremolque = document.getElementById("placa_semirremolque").value;
+  var nombre_operador = document.getElementById("nombre_operador").value;
+  var celular_operador = document.getElementById("celular_operador").value;
+  var tipo_unidad = document.getElementById("tipo_unidad").value;
+  var tipo_unidadparticular = document.getElementById("tipo_unidadparticular").value;
+  var estado_carga = document.getElementById("estado_carga").value;
+  var tipo_carga = document.getElementById("tipo_carga").value;
+  var origen = document.getElementById("origen").value;
+  var destino = document.getElementById("destino").value;
+  var fecha_ocurrencia = document.getElementById("fecha_ocurrencia").value;
+  var hora_ocurrencia = document.getElementById("hora_ocurrencia").value;
+  var fecha_reinicio = document.getElementById("fecha_reinicio").value;
+  var hora_reinicio = document.getElementById("hora_reinicio").value;
+  var tiempo_detenido = document.getElementById("tiempo_detenido").value;
+  var tipo_falla = document.getElementById("tipo_falla").value;
+  var lugar_ocurrencia = document.getElementById("lugar_ocurrencia").value;
+  var zona = document.getElementById("zona").value;
+  var sede = document.getElementById("sede").value;
+  var region = document.getElementById("region").value;
+  var provincia = document.getElementById("provincia").value;
+  var distrito = document.getElementById("distrito").value;
+  var latitud = document.getElementById("latitud").value;
+  var longitud = document.getElementById("longitud").value;
+  var inicio_tramo = document.getElementById("inicio_tramo").value;
+  var fin_tramo = document.getElementById("fin_tramo").value;
+  var personal_informante = document.getElementById("personal_informante").value;
+  var gestor = document.getElementById("gestor").value;
+  var fecha_recepcion_informacion = document.getElementById("fecha_recepcion_informacion").value;
+  var hora_recepcion_informacion = document.getElementById("hora_recepcion_informacion").value;
+  var ubicacion_actual = document.getElementById("ubicacion_actual").value;
+  var detalle_clima = document.getElementById("detalle_clima").value;
+
+  var zona_peligrosa = document.getElementById("Zona_Peligrosa_Si").checked
+    ? "SI"
+    : document.getElementById("Zona_Peligrosa_No").checked
       ? "NO"
       : "";
 
-    var supervisores_informados = document.getElementById(
-      "supervisores_informados"
-    ).value;
-    var seguimiento_falla = document.getElementById("seguimiento_falla").value;
-    var controlador_gps = document.getElementById("controlador_gps").value;
-    var cargo_area = document.getElementById("cargo_area").value;
-    var fecha_reporte = document.getElementById("fecha_reporte").value;
-    var turno = document.getElementById("turno").value;
+  var supervisores_informados = document.getElementById("supervisores_informados").value;
+  var seguimiento_falla = document.getElementById("seguimiento_falla").value;
+  var controlador_gps = document.getElementById("controlador_gps").value;
+  var cargo_area = document.getElementById("cargo_area").value;
+  var fecha_reporte = document.getElementById("fecha_reporte").value;
+  var turno = document.getElementById("turno").value;
 
-    const respuesta = await fetch(
-      "https://sheet.best/api/sheets/0503114c-fd14-429d-8a17-960266cfedac",
-      {
-        method: "POST",
-        mode: "cors",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          codigo_unidad: codigo_unidad,
-          placa_remolque: placa_remolque,
-          codigo_acople: codigo_acople,
-          placa_semirremolque: placa_semirremolque,
-          nombre_operador: nombre_operador,
-          celular_operador: celular_operador,
-          tipo_unidad: tipo_unidad,
-          tipo_unidadparticular: tipo_unidadparticular,
-          estado_carga: estado_carga,
-          tipo_carga: tipo_carga,
-          origen: origen,
-          destino: destino,
-          fecha_ocurrencia: fecha_ocurrencia,
-          hora_ocurrencia: hora_ocurrencia,
-          fecha_reinicio: fecha_reinicio,
-          hora_reinicio: hora_reinicio,
-          tiempo_detenido: tiempo_detenido,
-          tipo_falla: tipo_falla,
-          lugar_ocurrencia: lugar_ocurrencia,
-          zona: zona,
-          sede: sede,
-          region: region,
-          provincia: provincia,
-          distrito: distrito,
-          latitud: latitud,
-          longitud: longitud,
-          inicio_tramo: inicio_tramo,
-          fin_tramo: fin_tramo,
-          personal_informante: personal_informante,
-          gestor: gestor,
-          fecha_recepcion_informacion: fecha_recepcion_informacion,
-          hora_recepcion_informacion: hora_recepcion_informacion,
-          ubicacion_actual: ubicacion_actual,
-          detalle_clima: detalle_clima,
-          zona_peligrosa: zona_peligrosa,
-          supervisores_informados: supervisores_informados,
-          seguimiento_falla: seguimiento_falla,
-          controlador_gps: controlador_gps,
-          cargo_area: cargo_area,
-          fecha_reporte: fecha_reporte,
-          turno: turno,
-        }),
-      }
-    );
-    console.log(respuesta);
+  const respuesta = await fetch(
+    'https://sheetdb.io/api/v1/sh9eom904m66a',
+    {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
 
-    document.getElementById("Principal_Form").style.display = "none";
+        data: [
+          {
+            'id': "INCREMENT",
+            'codigo_unidad': codigo_unidad,
+            'placa_remolque': placa_remolque,
+            'codigo_acople': codigo_acople,
+            'placa_semirremolque': placa_semirremolque,
+            'nombre_operador': nombre_operador,
+            'celular_operador': celular_operador,
+            'tipo_unidad': tipo_unidad,
+            'tipo_unidadparticular': tipo_unidadparticular,
+            'estado_carga': estado_carga,
+            'tipo_carga': tipo_carga,
+            'origen': origen,
+            'destino': destino,
+            'fecha_ocurrencia': fecha_ocurrencia,
+            'hora_ocurrencia': hora_ocurrencia,
+            'fecha_reinicio': fecha_reinicio,
+            'hora_reinicio': hora_reinicio,
+            'tiempo_detenido': tiempo_detenido,
+            'tipo_falla': tipo_falla,
+            'lugar_ocurrencia': lugar_ocurrencia,
+            'zona': zona,
+            'sede': sede,
+            'region': region,
+            'provincia': provincia,
+            'distrito': distrito,
+            'latitud': latitud,
+            'longitud': longitud,
+            'inicio_tramo': inicio_tramo,
+            'fin_tramo': fin_tramo,
+            'personal_informante': personal_informante,
+            'gestor': gestor,
+            'fecha_recepcion_informacion': fecha_recepcion_informacion,
+            'hora_recepcion_informacion': hora_recepcion_informacion,
+            'ubicacion_actual': ubicacion_actual,
+            'detalle_clima': detalle_clima,
+            'zona_peligrosa': zona_peligrosa,
+            'supervisores_informados': supervisores_informados,
+            'seguimiento_falla': seguimiento_falla,
+            'controlador_gps': controlador_gps,
+            'cargo_area': cargo_area,
+            'fecha_reporte': fecha_reporte,
+            'turno': turno,
+          }
+        ]
+      })
+    }).then((response) => response.json())
+    .then((data) => console.log(data));
 
-    const successMessage = document.getElementById("Registro_Exitoso");
-    successMessage.style.display = "flex"; // Cambiamos de 'block' a 'flex' para mantener el diseño
-    successMessage.style.opacity = "0"; // Inicializamos la opacidad para la animación
-    setTimeout(function () {
-      successMessage.style.opacity = "1";
-      successMessage.style.transform = "translateY(0)";
-    }, 5);
-  }
+  document.getElementById("Principal_Form").style.display = "none";
+
+  const successMessage = document.getElementById("Registro_Exitoso");
+  successMessage.style.display = "flex"; // Cambiamos de 'block' a 'flex' para mantener el diseño
+  successMessage.style.opacity = "0"; // Inicializamos la opacidad para la animación
+  setTimeout(function () {
+    successMessage.style.opacity = "1";
+    successMessage.style.transform = "translateY(0)";
+  }, 5);
 }
 
 function Previsualizar() {
@@ -286,8 +280,8 @@ function Previsualizar() {
   var zona_peligrosa = document.getElementById("Zona_Peligrosa_Si").checked
     ? "SI"
     : document.getElementById("Zona_Peligrosa_No").checked
-    ? "NO"
-    : "";
+      ? "NO"
+      : "";
 
   var supervisores_informados = document.getElementById(
     "supervisores_informados"
@@ -304,7 +298,7 @@ function Previsualizar() {
   <table id="table2">
     <tr>
       <td colspan="1">
-        <img src="logorac.png" alt="" width="70%" height="70%">
+        <img src="logorac.png" alt="">
       </td>
       <td colspan="4">
         <h2 style="text-align: center">
@@ -410,10 +404,9 @@ function Previsualizar() {
         ${longitud}
       </td>
       <td>
-        <label for="">Inicio Tramo</label>
+        <label for="">Tramo </label>
         ${inicio_tramo}
-        <br>
-        <label for="">Fin de Tramo</label>
+        <label for="">-</label>
         ${fin_tramo}
       </td>
     </tr>
@@ -445,28 +438,28 @@ function Previsualizar() {
 
     <tr class="section-title">
       <td colspan="3">PERSONAS A LAS QUE SE INFORMÓ</td>
-      <td colspan="5">DETALLES</td>
+      <td colspan="4">DETALLES</td>
     </tr>
 
     <tr>
       <th colspan="3">
         ${supervisores_informados}
-      <td colspan="5">
+      <td colspan="4">
         ${seguimiento_falla}
       </td>
       </th>
     </tr>
     <tr>
-      <td colspan="8"><label for="">Fotos:</label></textarea></td>
+      <td colspan="7"><label for="">Fotos:</label></textarea></td>
     </tr>
     <tr>
-      <th colspan="8">REPORTADO POR:</th>
+      <th colspan="7">REPORTADO POR:</th>
     </tr>
     <tr>
       <th colspan="2">NOMBRE</th>
       <th colspan="1">CARGO</th>
-      <th>FECHA (año / mes / día)</th>
-      <th>TURNO</th>
+      <th colspan="1">FECHA (año / mes / día)</th>
+      <th colspan="1">TURNO</th>
       <th colspan="2">FIRMA</th>
     </tr>
     <tr>
@@ -481,8 +474,8 @@ function Previsualizar() {
       </td>
       <td colspan="1">
       ${turno}
-        <td colspan="2">
-        </td>
+      <td colspan="2">
+        -
       </td>
     </tr>
   </table>
